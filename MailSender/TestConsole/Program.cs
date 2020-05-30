d64d455b1aa75e158dead802e9ee4a7472d1c2a4
but-sergey <but-sergey@bk.ru>
2020-05-30 19:41:45 +0300

    Добавлено консольное приложение с проверкой отправки электронного письма

---
+++ MailSender/TestConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mailMessage = new MailMessage(Settings.FROM_MAIL, Settings.TO_MAIL);
+            mailMessage.Subject = "Пробное письмо";
+            mailMessage.Body = "Содержимое пробного письма";
+            mailMessage.IsBodyHtml = false;
+
+            SmtpClient client = new SmtpClient(Settings.SMTP_SERVER, Settings.SMTP_PORT);
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            var password = Settings.SENDER_PASSWORD;
+            if (password == string.Empty)
+            {
+                Console.Write("Пароль для доступа к аккаунту отправки писем: ");
+                password = Console.ReadLine();
+            }
+            client.Credentials = new NetworkCredential(Settings.SENDER_NAME, password);
+
+            try
+            {
+                client.Send(mailMessage);
+                Console.WriteLine($"Письмо от {Settings.FROM_MAIL} на адрес {Settings.TO_MAIL} отправлено.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Невозможно отправить письмо ({ex.Message})");
+            }
+
+            Console.Write("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MailSender/TestConsole/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace TestConsole
+{
+    static class Settings
+    {
+        public static string SMTP_SERVER = "smtp.mail.ru";
+        //public static string SMTP_SERVER = "smtp.yandex.ru";
+        public static int SMTP_PORT = 25;
+        //public static int SMTP_PORT = 465;
+
+        //public static string FROM_MAIL = "asketemius@yandex.ru";
+        public static string FROM_MAIL = "test-3005@list.ru";
+        public static string TO_MAIL = "but-sergey@bk.ru";
+        //public static string TO_MAIL = "asketemius@yandex.ru";
+
+        public static string SENDER_NAME = "test-3005@list.ru";
+        //public static string SENDER_NAME = "but-sergey@bk.ru";
+        //public static string SENDER_NAME = "asketemius";
+
+        public static string SENDER_PASSWORD = "";
+    }
+}
