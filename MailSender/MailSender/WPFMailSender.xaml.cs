155d1c56d6821fd8fdad806a3cf43e14354b788e
but-sergey <but-sergey@bk.ru>
2020-06-05 15:01:50 +0300

    Добавлены классы и обработчики для отложенной отправки писем

--- MailSender/MailSender/WPFMailSender.xaml.cs
+++ MailSender/MailSender/WPFMailSender.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MailSender.Services;
 using MailSender.StaticClasses;
+using System;
 using System.Linq;
 using System.Windows;
 
@@ -46,6 +47,27 @@ namespace MailSender
 
             EmailSendService emailSender = new EmailSendService(strLogin, strPassword);
             emailSender.SendMails((IQueryable<Email>)dgEmails.ItemsSource);
+            MessageBox.Show("Письма отправлены", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            Scheduler scheduler = new Scheduler();
+            TimeSpan tsSendTime = scheduler.GetSendTime(tbTimePicker.Text);
+            if(tsSendTime == new TimeSpan())
+            {
+                MessageBox.Show("Некорректный формат даты");
+                return;
+            }
+            DateTime dtSendDateTime = (cldSchedulDateTimes.SelectedDate ?? DateTime.Today).Add(tsSendTime);
+            if(dtSendDateTime < DateTime.Now)
+            {
+                MessageBox.Show("Дата и время отправки писем не могут быть раньше, чем настоящее время");
+                return;
+            }
+            EmailSendService emailSender = new EmailSendService(cbSenderSelect.Text, 
+                                                        cbSenderSelect.SelectedValue.ToString());
+            scheduler.SendEmails(dtSendDateTime, emailSender, (IQueryable<Email>)dgEmails.ItemsSource);
         }
     }
 }
