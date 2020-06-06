027be9ed650df55dae8be9836c1b7883511a5357
but-sergey <but-sergey@bk.ru>
2020-06-06 10:45:02 +0300

    Добавлено подключение к файлу базы данных (вместо подключения к SQL-серверу)

--- MailSender/MailSender/Properties/Settings.Designer.cs
+++ MailSender/MailSender/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace MailSender.Properties {
                 return ((string)(this["MailsAndSendersConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MailsAndSende" +
+            "rs.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string MailsAndSendersConnectionString1 {
+            get {
+                return ((string)(this["MailsAndSendersConnectionString1"]));
+            }
+        }
     }
 }
